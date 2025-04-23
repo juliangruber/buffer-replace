b9d454d5eae82da3eef863bf71435be12ef51b2d
eli sharf <57587340+esharf@users.noreply.github.com>
2025-04-23 23:57:18 +0300

    tests fix

--- test.js
+++ test.js
@@ -2,21 +2,21 @@ const replace = require('.');
 const test = require('tap').test;
 
 test('simple', t => {
-  t.deepEqual(
-    replace(Buffer('foo:bar'), ':', '-'),
-    Buffer('foo-bar')
+  t.same(
+    replace(Buffer.from('foo:bar'), ':', '-'),
+    Buffer.from('foo-bar')
   );
-  t.deepEqual(
-    replace(Buffer('foo:beep:bar'), ':beep:', '-'),
-    Buffer('foo-bar')
+  t.same(
+    replace(Buffer.from('foo:beep:bar'), ':beep:', '-'),
+    Buffer.from('foo-bar')
   );
-  t.deepEqual(
+  t.same(
     replace('foo:beep:bar', ':beep:', '-'),
-    Buffer('foo-bar')
+    Buffer.from('foo-bar')
   );
-  t.deepEqual(
-    replace(Buffer('foo:beep:bar'), 'nope', '-'),
-    Buffer('foo:beep:bar')
+  t.same(
+    replace(Buffer.from('foo:beep:bar'), 'nope', '-'),
+    Buffer.from('foo:beep:bar')
   );
   t.end();
 });
