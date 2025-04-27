68985b1ca6fdfc0ccf4100b57c02705743586c85
eli sharf <57587340+esharf@users.noreply.github.com>
2025-04-27 19:46:03 +0300

    aded a test for `RangeError: Maximum call stack size exceeded`

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
 const replace = require('.');
 const test = require('tap').test;
 
+let maxStackSize = 0;
+
+function measureStackSize() {
+  maxStackSize++;
+  measureStackSize();
+}
+
+try {
+  measureStackSize();
+} catch { }
+
 test('simple', t => {
   t.same(
     replace(Buffer.from('foo:bar'), ':', '-'),
@@ -18,7 +29,9 @@ test('simple', t => {
     replace(Buffer.from('foo:beep:bar'), 'nope', '-'),
     Buffer.from('foo:beep:bar')
   );
+  t.same(
+    replace(Buffer.from(Array(maxStackSize + 1000).fill('ab').join()), 'a', 'b'),
+    Buffer.from(Buffer.from(Array(maxStackSize + 1000).fill('bb').join()))
+  );
   t.end();
 });
-
-
